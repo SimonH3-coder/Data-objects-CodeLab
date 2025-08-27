fc5499946e292ce7d5ff13932f3912890dd3b5c8
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-08-27 09:12:54 +0200

    done

--- opgavefiler/js/easy.js
+++ opgavefiler/js/easy.js
@@ -1,18 +1,26 @@
 /* opgave 1 */
 console.group("opgave 1");
 
+const person = {
+  name: "Ivan",
+  age: "39",
+  job: "Maler",
+};
+
+console.log(person);
+
 console.groupEnd();
 
 /* opgave 2 */
 console.group("opgave 2");
 
-
+console.log(person.name); //"Ivan"
+console.log(person.job); //"Maler"
+console.log(`${person.name} job arbejder som er ${person.job}`);
 
 console.groupEnd();
 
 /* opgave 3 */
 console.group("opgave 3");
-
-
 
 console.groupEnd();
